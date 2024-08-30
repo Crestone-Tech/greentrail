bf893ce454b9ecd3e3fd00cdbf7a1acb9bd1d098
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-30 15:46:30 -0700

    adds effects.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./assets/css/effects.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
