3bfca2c51619e256998403d013eb983a4f950125
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-30 11:42:34 -0700

    adds GitHubLink

---
+++ client/src/components/GitHubLink/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./git-hub-link.css";
+
+export default function GitHubLink() {
+  return (
+    <>
+      <a href="https://github.com/Crestone-Tech/greentrail" target="_blank">
+        <i className="fa fa-github light"></i>
+      </a>
+    </>
+  );
+}
