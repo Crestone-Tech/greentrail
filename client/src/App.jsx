06297d76bba62e094486291e172f7285264fc3e1
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-30 11:28:47 -0700

    adds React imports and rough footer HTML

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import Header from "./components/Header";
 import "./App.css";
@@ -8,6 +9,14 @@ function App() {
   return (
     <>
       <Header />
+      <footer id="contact">
+        <a
+          href="https://github.com/crestonesoftware/greentrail"
+          target="_blank"
+        >
+          <i className="fa fa-github light"></i>
+        </a>
+      </footer>
     </>
   );
 }
