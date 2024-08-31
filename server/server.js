9405d2bad7f3706321d5d7a9e83e735d76bc3ac0
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-31 09:13:16 -0700

    added bare bones of server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+//TODO const routes = require("./controllers");
+const sequelize = require("./config/connection");
+//TODO const helpers = require("./utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+//app.use(routes);
+
+//TODO const helpers = require("./utils/helpers");
+//sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => console.log("Now listening on port", PORT));
+// });
