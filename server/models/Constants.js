109c9651332f2329387e2ca32e9775779625765c
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-31 10:18:38 -0700

    adds models and seeds

---
+++ server/models/Constants.js
@@ -0,0 +1,17 @@
+const TABLES = {
+  // Location tables
+  COUNTRY: "country",
+  PROVINCE: "province",
+  TOWN: "town",
+  SITE: "site",
+
+  // Community and Provider tables
+  COMMUNITY: "community",
+  PROVIDER: "provider",
+  PROVIDER_TAG: "provider_tag",
+  TAG: "tag",
+  EVENT: "event",
+  EVENT_SERIES: "event_series",
+};
+
+module.exports = { TABLES };
