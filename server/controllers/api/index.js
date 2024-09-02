1c9229b02655ba10255c67c198b44397bf8e64ca
cuhrmunken <elliott.davis@crestonetech.com>
2024-09-01 17:27:35 -0700

    added user routes

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -6,14 +6,14 @@ const router = require("express").Router();
 // const eventRoutes = require("./event-routes.js");
 // const eventSeriesRoutes = require("./eventSeries-routes.js");
 const communityRoutes = require("./community-routes.js");
-// const userRoutes = require("./user-routes");
+const userRoutes = require("./user-routes");
 // const providerRoutes = require("./provider-routes");
 
 // router.use("/locations", locationRoutes);
 // router.use("/events", eventRoutes);
 // router.use("/eventSeries", eventSeriesRoutes);
 // router.use("/locations", locationRoutes);
-// router.use("/users", userRoutes);
+router.use("/users", userRoutes);
 router.use("/communities", communityRoutes);
 // router.use("/providers", providerRoutes);
 
